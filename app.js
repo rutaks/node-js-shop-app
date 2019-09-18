f104aa86b58ad18fd72821d2caff98db8971f57a
rutaks <rootsum.dev@gmail.com>
2019-09-18 06:51:17 +0100

    Added Routes

--- app.js
+++ app.js
@@ -2,6 +2,11 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 
+const adminRoute = require('./routes/admin');
+const shopRoute = require('./routes/shop');
+
 app.use(bodyParser.urlencoded({extended : true}));
+
+app.use('/', adminRoute);
 
 app.listen(3000);
