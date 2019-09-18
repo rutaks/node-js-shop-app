048669b2f6b719f679584845119a0611a5b8c813
rutaks <rootsum.dev@gmail.com>
2019-09-18 07:11:43 +0100

    Add Some Few Pages

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const shopRoute = require('./routes/shop');
 
 app.use(bodyParser.urlencoded({extended : true}));
 
-app.use('/', adminRoute);
+app.use('/admin', adminRoute);
 
 app.use((req,res,next)=>{
     res.status(404).send('<h1>404 Page Not Found</h1>')
