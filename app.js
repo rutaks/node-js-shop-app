5462b285fb18697b03886b9e9414d05647357c0f
rutaks <rootsum.dev@gmail.com>
2019-09-19 07:08:42 +0100

    Renamed Routes

--- app.js
+++ app.js
@@ -1,22 +1,22 @@
-const express = require('express');
+const express = require("express");
+const bodyParser = require("body-parser");
+const path = require("path");
+const rootDir = require("./util/path");
 const app = express();
-const bodyParser = require('body-parser');
-const path = require('path');
-const rootDir = require('./util/path');
+const errorController = require("./controller/error");
 
-const errorController = require('./controller/error');
-const adminRoute = require('./routes/admin');
-const shopRoute = require('./routes/shop');
+app.set("view engine", "ejs");
+app.set("views", "views");
 
-app.set('view engine', 'ejs');
-app.set('views', 'views');
+const adminRoutes = require("./routes/admin");
+const shopRoutes = require("./routes/shop");
 
-app.use(bodyParser.urlencoded({extended : true}));
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, "public")));
 
-app.use('/admin', adminRoute);
-app.use('/shop', shopRoute);
+app.use("/admin", adminRoutes);
+app.use(shopRoutes);
 
-app.use(errorController.get404)
+app.use(errorController.get404);
 
 app.listen(3000);
