5651397d962265c6ca32d4a1376e89f172823248
rutaks <rootsum.dev@gmail.com>
2019-09-22 05:38:23 +0100

    Added Find Product To Edit & Edit Product Functions To Routes

--- routes/admin.js
+++ routes/admin.js
@@ -12,4 +12,8 @@ router.get("/add-product", adminController.getAddProductPage);
 // /admin/add-product => POST {receives & processes submission of produc}
 router.post("/add-product", adminController.addProduct);
 
+router.get("/edit-product/:id", adminController.getEditProduct);
+
+router.post("/edit-product/", adminController.editProduct);
+
 module.exports = router;
