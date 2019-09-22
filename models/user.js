d8931d83b39d687bc19db854469491ef7e995627
rutaks <rootsum.dev@gmail.com>
2019-09-22 05:56:27 +0100

    Added User Model

---
+++ models/user.js
@@ -0,0 +1,15 @@
+const Sequelize = require("sequelize"),
+  sequelize = require("../util/database");
+
+const User = sequelize.define("user", {
+  id: {
+    type: Sequelize.INTEGER,
+    autoIncrement: true,
+    allowNull: false,
+    primaryKey: true
+  },
+  email: Sequelize.STRING,
+  name: Sequelize.STRING
+});
+
+module.exports = User;
