f061a98423d9a1dd5c8d0ae427e0d7e233432498
rutaks <samrutakayile@awesomity.rw>
2019-10-24 05:25:19 +0200

    Changed Dependencies

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const bodyParser = require("body-parser");
 const path = require("path");
 const rootDir = require("./util/path");
 const app = express();
-const sequelize = require("./util/database");
+const mongoose = require("mongoose");
 const errorController = require("./controller/error");
 
 const Product = require("./models/product");
@@ -34,29 +34,3 @@ app.use("/admin", adminRoutes);
 app.use(shopRoutes);
 
 app.use(errorController.get404);
-
-Product.belongsTo(User, { constraints: true, onDelete: "CASCADE" });
-User.hasMany(Product);
-
-User.hasOne(Cart);
-Cart.belongsTo(User, { constraints: true, onDelete: "CASCADE" });
-
-Cart.belongsToMany(Product, { through: CartItem });
-Product.belongsToMany(Cart, { through: CartItem });
-
-sequelize
-  .sync({ force: true })
-  .then(result => {
-    return User.findByPk(1);
-  })
-  .then(user => {
-    if (!user)
-      return User.create({ name: "Samuel Rutakayile", email: "fake@fake.com" });
-    else return user;
-  })
-  .then(user => {
-    app.listen(3000);
-  })
-  .catch(err => {
-    console.log(err);
-  });
