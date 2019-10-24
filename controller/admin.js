f622c183323ef901e5034942c92321eaa2e6de79
rutaks <samrutakayile@awesomity.rw>
2019-10-24 07:03:18 +0200

    Refactored getProduct(s) controller

--- controller/admin.js
+++ controller/admin.js
@@ -7,54 +7,56 @@ exports.getAddProductPage = (req, res, next) => {
 };
 
 exports.addProduct = (req, res, next) => {
-  const title = req.body.title;
-  const imgUrl = req.body.imgUrl;
-  const price = req.body.price;
-  const description = req.body.description;
-  req.user
-    .createProduct({
-      title: title,
-      price: price,
-      imageUrl: imgUrl,
-      description: description
-    })
+  const { title, imgUrl, price, description } = req.body;
+  const product = new Product({
+    title: title,
+    price: price,
+    description: description,
+    imgUrl: imgUrl
+  });
+  product
+    .save()
     .then(result => {
       res.redirect("/admin/products");
     })
     .catch(err => {
-      console.log(err);
+      console.log("ERR: Could not save Product");
     });
 };
 
 exports.getProducts = (req, res, next) => {
-  req.user
-    .getProducts()
+  Product.find()
     .then(products => {
-      res.render("admin/Products", {
+      res.render("admin/products", {
         products: products,
         pageTitle: "Admin Products"
       });
     })
     .catch(err => {
-      console.log("An Error Occured");
+      console.log("ERR: Could Not Get Products");
     });
 };
 
+exports.getProduct = (req, res, next) => {
+  const productId = req.params.id;
+  Product.findById(productId).then(product => {
+    res.render("shop/product-details", {
+      product: product,
+      pageTitle: "Product Details"
+    });
+  });
+};
+
 exports.getEditProduct = (req, res, next) => {
-  const id = req.params.id;
-  req.user
-    .getProducts({ where: { id: id } })
-    .then(products => {
-      const product = products[0];
-      if (!product) {
-        res.redirect("/");
-      }
+  const productId = req.params.id;
+  Product.findById(productId)
+    .then(product => {
       res.render("admin/edit-product", {
         pageTitle: "Edit Product",
         product: product
       });
     })
-    .catch(err => console.log(err));
+    .catch(err => console.log("ERR: Could not find Product, " + err));
 };
 
 exports.editProduct = (req, res, next) => {
