441451d3852c4374fa02f051a3b04da07a2e2f62
rutaks <samrutakayile@awesomity.rw>
2019-10-25 05:55:46 +0200

    Added new Create Order Route

--- routes/shop.js
+++ routes/shop.js
@@ -14,7 +14,10 @@ router.get("/cart", shopController.getCart);
 router.post("/cart", shopController.postCart);
 
 router.post("/cart-delete-item", shopController.deleteCartProduct);
-// router.get("/orders", shopController.getOrders);
+
+router.get("/orders", shopController.getOrders);
+
+router.post("/create-order", shopController.postOrders);
 
 // router.get("/checkout", shopController.getCheckout);
 
