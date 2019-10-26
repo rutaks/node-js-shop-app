bcb35191835afc08746e440e8ad4a365e5478a06
rutaks <samrutakayile@awesomity.rw>
2019-10-26 10:27:55 +0200

    Implemented Session Storage Of User functions

--- app.js
+++ app.js
@@ -38,7 +38,8 @@ app.use(
 );
 
 app.use((req, res, next) => {
-  User.findById("5db167aa85c99f4d390fd76d")
+  if (!req.session.user) return next();
+  User.findById(req.session.user._id)
     .then(user => {
       req.user = user;
       next();
