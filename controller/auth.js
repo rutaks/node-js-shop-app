20cc42833f9c8d00ddd4752e338c4915fe135f30
rutaks <samrutakayile@awesomity.rw>
2019-10-26 10:00:35 +0200

    Added login function

--- controller/auth.js
+++ controller/auth.js
@@ -1,3 +1,17 @@
+const User = require("../models/user");
+
 exports.getLoginPage = (req, res, next) => {
   res.render("auth/login", { pageTitle: "Login" });
+};
+
+exports.login = (req, res, next) => {
+  User.findById("5db167aa85c99f4d390fd76d")
+    .then(user => {
+      req.session.isLoggedIn = true;
+      req.session.user = user;
+      res.redirect("/");
+    })
+    .catch(err => {
+      console.log("ERR: Could Not Login, " + err);
+    });
 };
